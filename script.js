fcab7b18004070dbb1cea0a0f5af393c4b0c2787
emon0121 <116112338+emon0121@users.noreply.github.com>
2026-02-07 17:52:53 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+let allProducts = [];
+
+fetch("products.json")
+.then(res => res.json())
+.then(data => {
+  allProducts = data;
+  showProducts(allProducts);
+});
+
+function showProducts(list){
+  const box = document.getElementById("productList");
+  box.innerHTML = "";
+
+  list.forEach(p => {
+    const card = document.createElement("div");
+    card.className = "product";
+
+    card.innerHTML = `
+      <img src="${p.image}">
+      <h4>${p.name}</h4>
+      <p>${p.price}</p>
+    `;
+
+    card.onclick = () => {
+      localStorage.setItem("product", JSON.stringify(p));
+      window.location.href = "product.html";
+    };
+
+    box.appendChild(card);
+  });
+}
+
+function filterCategory(cat){
+  if(cat === "All"){
+    showProducts(allProducts);
+  } else {
+    showProducts(allProducts.filter(p => p.category === cat));
+  }
+}
